a83cbbd3dc268ac6c67102061b3235e7c84e7377
Anik <sikder35-1408@diu.edu.bd>
2022-04-25 21:52:30 +0600

    array concat, slice

--- script5.js
+++ script5.js
@@ -1,3 +1,30 @@
 
 const arr1 = [1,2,3];
 const arr2 = [4,5,6];
+
+// Concat multiple arrays
+const arr3 = arr1.concat(arr2);
+console.log(arr3);
+
+// Using spread
+const arr4 = [...arr1, ...arr2];
+console.log(arr4);
+
+const arr5 = [].concat(arr1, arr2);
+console.log(arr5);
+//----------------------------------------------------------
+
+
+// Usig slice() method
+const arr = [1,2,3,4,5,6];
+
+// Slice
+const sliced = arr.slice(1,3);
+console.log(sliced);
+
+// Clone an array
+const cloned = arr.slice();
+console.log(cloned);
+
+// Changing array like objects to array
+
